af3b714dc39b42a718ddd4f288f883e97ea8340a
Outsource1498 <outsource1498@users.noreply.github.com>
2026-07-30 11:43:35 +0300

    Add forward declarations for %new methods to fix compiler error

--- headers/YouTube/YTMSettingsViewController.h
+++ headers/YouTube/YTMSettingsViewController.h
@@ -4,3 +4,11 @@
 // YouTube Music Settings view controller
 @interface YTMSettingsViewController : UIViewController
 @end
+
+// Forward declarations for %new methods added by our tweak
+@interface YTMSettingsViewController (LFMPatch)
+- (UIView *)lfm_findScrollViewInView:(UIView *)view excluding:(UIView *)exclude;
+- (void)lfm_updateStatusLabel;
+- (void)lfm_userDefaultsChanged:(NSNotification *)note;
+- (void)lfm_settingsTapped;
+@end
